299108d12a1e77d1c559ffcabd0e3a2ff2680f3c
Mr.Mikki <60034555+MrMikki-boop@users.noreply.github.com>
2023-03-22 18:59:45 +0300

    Adding comments

--- src/testKaAc.java
+++ src/testKaAc.java
@@ -5,18 +5,18 @@ import static java.lang.Integer.parseInt;
 
 
 public class testKaAc {
-    static Scanner console = new Scanner(System.in);
-    static boolean normalMode = false;
+    static Scanner console = new Scanner(System.in); // Ввод любых данных
+    static boolean normalMode = false; // Мод с ограничениями
 
     public static void main (String[] args) {
         System.out.println("Введите арабские или римские цифры ");
         while(true) {
             System.out.print("Выражение: ");
-            String uInput = console.nextLine();
-            if(uInput.contains("stop"))
+            String uInput = console.nextLine(); // Ввод для String-данных
+            if(uInput.contains("stop")) // Если консоль написано это, то
             {
                 System.out.println("Выход из приложения...");
-                System.exit(0);
+                System.exit(0); // Функцию выхода
             }
             if(uInput.contains("normal"))
             {
@@ -29,27 +29,27 @@ public class testKaAc {
 
 
     public static String calc(String input) {
-        char n = '+';
-        int result;
-        char[] uChar = new char[10];
-        for (int i = 0; i < input.length(); i++) {
-            if (i >= uChar.length) break;
-            uChar[i] = input.charAt(i);
-            if (uChar[i] == '+') n = '+';
+        var n = '+'; // Инициализация переменной со значением '+'
+        int result; // Объявление переменной
+        char[] uChar = new char[10]; // Создание массива с размером 10
+        for (int i = 0; i < input.length(); i++) { // Цикл for. Будет выполняться, пока переменная i меньше длины строки input.
+            if (i >= uChar.length) break; // Если значение переменной i больше или равно длине массива uChar, то цикл прерывается
+            uChar[i] = input.charAt(i); // Присваивание элементу массива uChar с индексом i значения символа строки input с индексом i.
+            if (uChar[i] == '+') n = '+';     // Если элемент массива i = '+', то перменной n присваивем '+'
             else if (uChar[i] == '-') n = '-';
             else if (uChar[i] == '*') n = '*';
             else if (uChar[i] == '/') n = '/';
         }
-        String uCharString = String.valueOf(uChar).toUpperCase();
-        String[] numbs = uCharString.split("[+-/*]");
-        if(numbs.length > 2) throw new RuntimeException("Формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
-        if(numbs.length < 2) throw new RuntimeException("Cтрока не является математической операцией");
-        String tmpNum1 = numbs[0].trim();
+        String uCharString = String.valueOf(uChar).toUpperCase(); // переводим массив uChar в верхний регистр
+        String[] numbs = uCharString.split("[+-/*]"); //Эта строка кода разбивает строку uCharString на подстроки, используя символы "+", "-", "*", "/" в качестве разделителей. Результатом является массив строк numbs, который содержит все подстроки, полученные в результате разбиения.
+        if(numbs.length > 2) throw new RuntimeException("Формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)"); // Если больше мат.операций
+        if(numbs.length < 2) throw new RuntimeException("Cтрока не является математической операцией"); // Если меньше
+        String tmpNum1 = numbs[0].trim(); // Массиво строки обрезается с обоих сторон от лишних пробелов
         String tmpNum2 = numbs[1].trim();
-        int romanNum1 = romanToArabic(tmpNum1);
+        int romanNum1 = romanToArabic(tmpNum1); // Присваивание tmpNum1 объект romanToArabic и называем это всё romanNum1
         int romanNum2 = romanToArabic(tmpNum2);
-        if((romanNum1 > 10 || romanNum2 > 10) && !normalMode) throw new RuntimeException("Доступны операции с числами от 1 до 10");
-        if(!(romanNum1 < 0 && romanNum2 < 0))
+        if((romanNum1 > 10 || romanNum2 > 10) && !normalMode) throw new RuntimeException("Доступны операции с числами от 1 до 10"); // Если первое или второе число > 10 и normalMode == false, то пишем исключени
+        if(!(romanNum1 < 0 && romanNum2 < 0)) // Tckb
         {
             if(romanNum1 < 0 || romanNum2 < 0) throw new RuntimeException("Используются одновременно разные системы счисления");
             result = Calculate(romanNum1, romanNum2, n);
@@ -67,13 +67,13 @@ public class testKaAc {
     {
         int result;
         switch (opera) {
-            case '+' -> result = num1 + num2;
+            case '+' -> result = num1 + num2; // -> — это лямбда-выражния - анонимная функция. Проще говоря, это метод без объявления
             case '-' -> result = num1 - num2;
             case '*' -> result = num1 * num2;
             case '/' -> {
                 try {
                     result = num1 / num2;
-                } catch (Exception e) {
+                } catch (Exception e) { //  это блок кода, который используется для обработки исключений, которые могут возникнуть при выполнении программы. Если в блоке try происходит ошибка, то управление передается в блок catch, где исключение обрабатывается. Параметр e типа Exception содержит информацию об ошибке, которая может быть использована для ее диагностики и исправления.
                     throw new RuntimeException("Нельзя делить на ноль");
                 }
             }
@@ -115,7 +115,7 @@ public class testKaAc {
 
         List<RomanNumeral> romanNumerals = RomanNumeral.getReverseSortedValues();
 
-        int i = 0;
+        var i = 0;
         StringBuilder sb = new StringBuilder();
 
         while ((number > 0) && (i < romanNumerals.size())) {
